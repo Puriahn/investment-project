08a2f8eb2ea96e0c4b4cb65352265a0d9933811b
Puriahn <119783351+Puriahn@users.noreply.github.com>
2024-07-18 17:06:38 +0330

    miss word investment fixed

--- src/Components/userInput.jsx
+++ src/Components/userInput.jsx
@@ -15,23 +15,34 @@ export default function UserInput({ onchange, input }) {
         </p>
         <p>
           <label>Annual Invesment</label>
-          <input type="number" required value={input.annualInvestment} onChange={(event) =>
-              onchange("annualInvesment", event.target.value)
-            }/>
+          <input
+            type="number"
+            required
+            value={input.annualInvestment}
+            onChange={(event) =>
+              onchange("annualInvestment", event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected Return</label>
-          <input type="number" required value={input.expectedReturn} onChange={(event) =>
-              onchange("expectedReturn", event.target.value)
-            }/>
+          <input
+            type="number"
+            required
+            value={input.expectedReturn}
+            onChange={(event) => onchange("expectedReturn", event.target.value)}
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required value={input.duration} onChange={(event) =>
-              onchange("duration", event.target.value)
-            }/>
+          <input
+            type="number"
+            required
+            value={input.duration}
+            onChange={(event) => onchange("duration", event.target.value)}
+          />
         </p>
       </div>
     </section>
